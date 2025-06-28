99fb29337e81bef435ed6d507cbbca2537910f1d
rikelmyso7 <rikelmyroberto1@gmail.com>
2025-06-27 22:35:59 -0300

    Day 1

---
+++ Day 1/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        //Ao imprimir um uma String é necessário utilizar "" pois assim ele imprime a string, caso utilize '' só poderá ser passado um char. ex: 'H'
+        System.out.println("Hello World");
+    };
+};
